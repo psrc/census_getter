ea8af0e30cb383c889df56bd8334ef3758515e0c
jkolberg <jwkolberg@gmail.com>
2026-01-06 15:57:05 -0800

    fix issue where tables would drop from h5
    - needed to be using the mode='a' instead of 'w'
    - the way it was looping inside of 'with pd.HDFStore' may have also been causing an issue

--- census_getter/steps/load_data.py
+++ census_getter/steps/load_data.py
@@ -5,17 +5,21 @@ from census_getter.util import Util
 def load_tables(util):
     # Creates an HDF5 file and loads tables into it
     table_list = util.get_table_list()
-    with pd.HDFStore(f"{util.get_data_dir()}/pipeline.h5", mode='w') as h5store:
-        for table in table_list:
-            print(f"Loading table: {table['tablename']} from file: {table['filename']}")
-            df = pd.read_csv(f"{util.get_data_dir()}/{table['filename']}",low_memory=False)
-            # fill nan values
-            df = util.fill_nan_values(df)
-            # check if block_group_id exists, if not create it
-            if not util.block_group_id_exists(df):
-                df = util.create_full_block_group_id(df)
-            else:
-                df = util.convert_col_to_int64(df, 'block_group_id')
+    for table in table_list:
+        print(f"Loading table: {table['tablename']} from file: {table['filename']}")
+        df = pd.read_csv(f"{util.get_data_dir()}/{table['filename']}",low_memory=False)
+        
+        # fill nan values
+        df = util.fill_nan_values(df)
+        
+        # check if block_group_id exists, if not create it
+        if not util.block_group_id_exists(df):
+            df = util.create_full_block_group_id(df)
+        else:
+            df = util.convert_col_to_int64(df, 'block_group_id')
+        
+        # save table to HDF5 store
+        with pd.HDFStore(f"{util.get_data_dir()}/pipeline.h5", mode='a') as h5store:
             h5store.put(table['tablename'], df, format='table')
 
 def run_step(context):
